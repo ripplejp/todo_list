38870b88589769b34bcaadf7493e1dc0d3324126
ripplejp <45304934+ripplejp@users.noreply.github.com>
2019-04-06 12:32:52 +0900

    Updates

---
+++ precache-manifest.873d51aed53fc710de46abec90fb68e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d3695f82e55517af57",
+    "url": "/todo_list/static/js/runtime~main.98d3695f.js"
+  },
+  {
+    "revision": "7e50246bdc9ac3fe3b28",
+    "url": "/todo_list/static/js/main.7e50246b.chunk.js"
+  },
+  {
+    "revision": "6f933aa54d70fbac70ff",
+    "url": "/todo_list/static/js/1.6f933aa5.chunk.js"
+  },
+  {
+    "revision": "7e50246bdc9ac3fe3b28",
+    "url": "/todo_list/static/css/main.8c8e78a6.chunk.css"
+  },
+  {
+    "revision": "0eb67d37bd1bc8d663fe9186ce886d93",
+    "url": "/todo_list/index.html"
+  }
+];
